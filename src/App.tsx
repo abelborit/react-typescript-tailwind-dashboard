d6f76dd3cf0b2887e432068da12b015b3c1787c0
Abel Borit <abelboritguitton@gmail.com>
2024-03-19 16:21:34 -0500

    [FEAT] create Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import "./App.css";
+import { Header } from "./components/Header";
 
 function App() {
   return (
     <>
-      <h1 className="text-3xl py-2 bg-cyan-600 text-center">
-        Vite + React + Tailwind
-      </h1>
+      <Header />
     </>
   );
 }
