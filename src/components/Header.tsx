7b3df5e72f90ffe777c0b9f3f82f361e4a633665
Abel Borit <abelboritguitton@gmail.com>
2024-03-20 13:04:21 -0500

    [STYLE] update Header and index.html

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,15 +2,15 @@ import { CheckboxTheme } from "./CheckboxTheme";
 
 export const Header = () => {
   return (
-    <header className="bg-Very-Pale-Blue dark:bg-Very-Dark-Blue h-[235px] rounded-b-[20px] pt-8 px-6 ">
+    <header className="bg-Very-Pale-Blue dark:bg-Very-Dark-Blue h-[260px] rounded-b-[20px] pt-8 px-6 shadow-md">
       <h1 className="text-Very-Dark-Blue dark:text-Very-Pale-Blue text-2xl font-bold mb-1">
         Social Media Dashboard
       </h1>
-      <p className="text-Dark-Grayish-Blue dark:text-Desaturated-Blue font-bold mb-5">
+      <p className="text-Dark-Grayish-Blue dark:text-Desaturated-Blue text-sm font-bold">
         Total Followers: 23,004
       </p>
 
-      <hr className="border-slate-500 mb-5" />
+      <hr className="border-slate-500 my-5" />
 
       <CheckboxTheme />
     </header>
